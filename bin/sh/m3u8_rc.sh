af84a64fa93879137bd1d0aafc463e284f4f4cdf
E9965-Kenny <e9965@nthykyldss.edu.hk>
2020-12-26 21:17:16 +0800

    Update

--- bin/sh/m3u8_rc.sh
+++ bin/sh/m3u8_rc.sh
@@ -17,7 +17,7 @@ do
     VNAME=$(openssl rand -hex 5)
     [[ ! -f ${MOVIEDIR}/movie/${VNAME}.mp4 ]] && break
 done
-downloadm3u8 -o ${MOVIEDIR}/movie/${VNAME}.mp4 --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66" -c 32 "${ORG_URL}"
+downloadm3u8 -o ${MOVIEDIR}/movie/${VNAME}.mp4 --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66" -c 16 "${ORG_URL}"
 if [[ $? == 0 ]]
 then
     rclone move ${MOVIEDIR}/movie/${VNAME}.mp4 ${DSET} -v --transfers=5 --cache-chunk-size 32M --no-traverse --create-empty-src-dirs --delete-empty-src-dirs --config "${RCLONE}"
