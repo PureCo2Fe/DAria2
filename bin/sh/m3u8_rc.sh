a846b8b473f894f1bb59dc9f2cf548015d540f8c
E9965-Kenny <e9965@nthykyldss.edu.hk>
2020-12-26 17:28:02 +0800

    Update

--- bin/sh/m3u8_rc.sh
+++ bin/sh/m3u8_rc.sh
@@ -13,11 +13,13 @@ do
     VNAME=$(openssl rand -hex 5)
     [[ ! -f ${MOVIEDIR}/${VNAME}.mp4 ]] && break
 done
-while true
-do
-    ffmpeg -i "${ORG_URL}" -bsf:a aac_adtstoasc -vcodec copy -c copy ${VNAME}.mp4 && break || rm -rf ${VNAME}* 
-done
-rclone move ${MOVIEDIR}/${VNAME}.mp4 ${DSET} -v --transfers=5 --cache-chunk-size 32M --no-traverse --create-empty-src-dirs --delete-empty-src-dirs --config "${RCLONE}"
-wait && find ${TEMP_UNZIP_PATH} -type d -empty -exec rm -rf {} \;
+ffmpeg -i "${ORG_URL}" -bsf:a aac_adtstoasc -vcodec copy -c copy ${VNAME}.mp4
+if [[ $? == 0 ]]
+then
+    rclone move ${MOVIEDIR}/${VNAME}.mp4 ${DSET} -v --transfers=5 --cache-chunk-size 32M --no-traverse --create-empty-src-dirs --delete-empty-src-dirs --config "${RCLONE}"
+    wait && find ${TEMP_UNZIP_PATH} -type d -empty -exec rm -rf {} \;
+else
+    rm -rf ${VNAME}*
+fi
 #-------------------------------------------------------------------
 IFS=$OLD_IFS
