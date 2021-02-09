106ccaa9c12cc925e902864b57496dff3fc1573d
E9965-Kenny <e9965@nthykyldss.edu.hk>
2021-02-09 17:26:46 +0800

    Update

--- ini.sh
+++ ini.sh
@@ -28,4 +28,4 @@ EOF
 wget --no-check-certificate -qO /datasets/DAria2.zip https://github.com/e9965/DAria2/blob/main/DAria2.zip?raw=true && unzip -qq /datasets/DAria2.zip -d /datasets/ && chmod +rwx /datasets/aria2.sh && chmod +rwx /datasets/sh.sh && rm -rf /datasets/DAria2.zip
 sudo bash /datasets/aria2.sh e9965
 ln -s /work/frp/frpc /bin/frpc
-frpc -c /work/frp/frpc.ini
+frpc -c /work/frp/frpc.ini & teleconsole
