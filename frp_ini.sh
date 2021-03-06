5e8313894220662c27c18abfa490e23bc7132f1f
E9965-Kenny <e9965@nthykyldss.edu.hk>
2021-03-06 21:25:06 +0800

    Update frp_ini.sh

--- frp_ini.sh
+++ frp_ini.sh
@@ -9,21 +9,21 @@ privilege_token = frp888
 type = tcp
 local_ip = 127.0.0.1
 local_port = 6800
-remote_port = 6800
+remote_port = ${1}
 #修改remote_port | 端口在：4000-50000 之间 | 建议改成难记的端口以免占用
-
-[ssrtcp]
-type = tcp
-local_ip = 127.0.0.1
-local_port = 10086
-remote_port = 6801
-#与Aria2不同
 
 [btaria2]
 type = tcp
 local_ip = 127.0.0.1
 local_port = 51413
 remote_port = 51413
+
+[ssrtcp]
+type = tcp
+local_ip = 127.0.0.1
+local_port = 10086
+remote_port = ${2}
+#与Aria2不同
 EOF
 ln -s /work/frp/frpc /bin/frpc
 frpc -c /work/frp/frpc.ini
